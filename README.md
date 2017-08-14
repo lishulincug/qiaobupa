ae8d290397b16efac97a98ba4362869135779c66
liangchaob <liangchaob@163.com>
2017-08-14 23:32:38 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # qiaobupa
+
+执行下面语句可以获取http://cv.qiaobutang.com/knowledge/categories下的所有文章,总计约5990篇
+```
+python request_categories.py
+```
+
+执行下面语句可以获取http://cv.qiaobutang.com/knowledge/intentions下的所有文章,总计约560篇
+```
+python request_intentions.py
+```
+
+执行过程实际是两步
+
+1. 获取全部链接
+2. 下载指定链接的标题内容
+
+返回数据为 json 格式,每一条数据都是title 和 content 的 kv形式,编码为 utf-8可能需要届时注意要再调整
---
+++ request_categories.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+* liangchaob@163.com 
+* 2017.7
+'''
+#设置中文字符
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+import requests
+from lxml import etree 
+import json
+
+
+import HTMLParser
+
+
+parser = HTMLParser.HTMLParser()
+
+
+#浏览器 header
+# 设置 header
+headers = {
+    'cache-control': "no-cache",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+
+
+# 获取该页超链接
+def get_categories(current_url):
+    r = requests.request("GET", current_url, headers=headers)
+    e_html = etree.HTML(r.text)
+    link_list = e_html.xpath('//a[@class="article_title__txt left"]/@href')
+    return link_list
+
+
+
+# 获取当前文章内容和标题
+def get_articles(current_url):
+    r = requests.request("GET", 'http://cv.qiaobutang.com'+current_url, headers=headers)
+    e_html = etree.HTML(r.text)
+    page_title = e_html.xpath('//div[@class="article__title"]/h1/text()')
+    page_title =  page_title[0]
+    # print page_title
+
+
+    page_content=e_html.xpath('//div[@class="article__body"]')
+    page_content = etree.tostring(page_content[0])
+    page_content = parser.unescape(page_content)
+
+    # print page_content
+
+    json_obj = {
+      'title': page_title,
+      'content': page_content
+    }
+
+    return json_obj
+
+
+
+
+
+
+# 获取 categories 下全部超链接
+def main():
+    total_link_list = []
+    for i in xrange(0,5990,10):
+        current_list = get_categories(current_url = 'http://cv.qiaobutang.com/knowledge/categories?skip='+str(i))
+        total_link_list.extend(current_list)
+
+        # 进度条
+        sys.stdout.write(str(i) +'/5990 链接已获取! \r')
+        sys.stdout.flush()
+
+
+
+    article_list = [] 
+    link_length = len(total_link_list)
+    g = 0
+    for link in total_link_list:
+        # 进度条
+        sys.stdout.write(str(g) +'/5990 页面已获取! \r')
+        sys.stdout.flush()
+        g = g+1
+
+        article_obj = get_articles(link)
+        jsonStr = json.dumps(article_obj, sort_keys=True, indent=2) 
+
+        with open('categories_articles.json','a') as wf:
+            wf.write(jsonStr)
+
+
+    print 'down!'
+
+
+if __name__ == '__main__':
+    main()
+
+
+
